0f45aa634077114f67376d29509dc0ff1f6ccfc6
Andrii Kuz <ext-andrii.kuz@here.com>
2017-12-22 16:43:13 +0200

    Create on repo for coursera crypto

--- scrooge_coin/target/debug/deps/ScroogeCoin-5dd6ee3da9a991c5.d
+++
@@ -1,9 +0,0 @@
-C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\target\debug\deps\ScroogeCoin-5dd6ee3da9a991c5.exe: src\lib.rs src\transaction.rs src\utxo.rs src\crypto.rs src\txhandler.rs
-
-C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\target\debug\deps\ScroogeCoin-5dd6ee3da9a991c5.d: src\lib.rs src\transaction.rs src\utxo.rs src\crypto.rs src\txhandler.rs
-
-src\lib.rs:
-src\transaction.rs:
-src\utxo.rs:
-src\crypto.rs:
-src\txhandler.rs:
--- scrooge_coin/target/rls/debug/deps/ScroogeCoin-c30f58f32d282c3c.d
+++
@@ -1,9 +0,0 @@
-c:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\target\rls\debug\deps\ScroogeCoin-c30f58f32d282c3c.rmeta: C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\src\lib.rs C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\src\transaction.rs C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\src\utxo.rs C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\src\crypto.rs C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\src\txhandler.rs
-
-c:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\target\rls\debug\deps\ScroogeCoin-c30f58f32d282c3c.d: C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\src\lib.rs C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\src\transaction.rs C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\src\utxo.rs C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\src\crypto.rs C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\src\txhandler.rs
-
-C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\src\lib.rs:
-C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\src\transaction.rs:
-C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\src\utxo.rs:
-C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\src\crypto.rs:
-C:\dev\crypto\coursera\coursera_bitcoin\scrooge_coin\src\txhandler.rs:
