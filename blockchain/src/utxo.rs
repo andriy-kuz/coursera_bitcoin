7bce8c55e4f16e4505a4da17cadc95e48ae70bb5
Andrii Kuz <ext-andrii.kuz@here.com>
2017-12-22 17:33:15 +0200

    Transit commit

---
+++ blockchain/src/utxo.rs
@@ -0,0 +1,45 @@
+use std::collections::HashMap;
+use super::transaction::TransactionOutput;
+
+#[derive(Eq, PartialEq, Hash)]
+pub struct UTXO {
+    hash: Vec<u8>,
+    index: usize,
+}
+
+impl UTXO {
+    pub fn new(hash: Vec<u8>, index: usize) -> UTXO {
+        UTXO { hash, index }
+    }
+}
+
+pub struct UTXOPool {
+    utxo_map: HashMap<UTXO, TransactionOutput>,
+}
+
+impl UTXOPool {
+    pub fn add_UTXO(&mut self, utxo: UTXO, tx_out: TransactionOutput) {
+        self.utxo_map.insert(utxo, tx_out);
+    }
+
+    pub fn remove_UTXO(&mut self, utxo: &UTXO) {
+        self.utxo_map.remove(utxo);
+    }
+
+    pub fn get_tx_out(&self, utxo: &UTXO) -> &TransactionOutput {
+        self.utxo_map.get(utxo).unwrap()
+    }
+
+    pub fn get_txs_out(&self) -> Vec<&TransactionOutput> {
+        let mut res: Vec<&TransactionOutput> = Vec::new();
+
+        for val in self.utxo_map.values() {
+            res.push(&val);
+        }
+        res
+    }
+
+    pub fn contains(&self, utxo: &UTXO) -> bool {
+        self.utxo_map.contains_key(utxo)
+    }
+}
