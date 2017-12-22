c4d88417f01b199ba44fe9adf626a6fb0d7d165f
Andrii Kuz <ext-andrii.kuz@here.com>
2017-12-22 16:41:56 +0200

    Transfer coursera tasks

---
+++ consensus_from_trust/target/rls/debug/deps/consensus_from_trust-b3dc55e566dfa99c.d
@@ -0,0 +1,10 @@
+c:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\target\rls\debug\deps\consensus_from_trust-b3dc55e566dfa99c.rmeta: C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\lib.rs C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\transaction.rs C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\candidate.rs C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\node.rs C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\malicious_node.rs C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\compliant_node.rs
+
+c:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\target\rls\debug\deps\consensus_from_trust-b3dc55e566dfa99c.d: C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\lib.rs C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\transaction.rs C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\candidate.rs C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\node.rs C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\malicious_node.rs C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\compliant_node.rs
+
+C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\lib.rs:
+C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\transaction.rs:
+C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\candidate.rs:
+C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\node.rs:
+C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\malicious_node.rs:
+C:\dev\crypto\coursera\coursera_bitcoin\consensus_from_trust\src\compliant_node.rs:
